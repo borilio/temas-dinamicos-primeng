8b3d016f33324a66f2e597b4df94b905afab38a5
borilio <exprimetuandroid@gmail.com>
2023-06-23 12:06:51 +0200

    Instaladas y configuradas las dependencias de PrimeNG, PrimeFlex y PrimeIcons

---
+++ src/app/primeng/primeng.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+
+import {RippleModule} from "primeng/ripple";
+
+
+
+@NgModule({
+
+  // Aquí solo exportaciones!!!!
+
+  exports: [
+    RippleModule
+  ]
+
+
+})
+export class PrimengModule { }
